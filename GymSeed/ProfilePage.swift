d6ae6041a51066ba6bc2bc299ac1d3b28a24f3c4
Abraham May <AbrahamMay1015@gmail.com>
2025-08-06 20:58:09 -0500

    creating Profile page

---
+++ GymSeed/ProfilePage.swift
@@ -0,0 +1,19 @@
+//
+//  ProfilePage.swift
+//  GymSeed
+//
+//  Created by Abraham may on 8/6/25.
+//
+
+import SwiftUI
+
+struct ProfilePage: View {
+    var body: some View {
+        OnboardingUploadProfile()
+        Spacer()
+    }
+}
+
+#Preview {
+    ProfilePage()
+}
